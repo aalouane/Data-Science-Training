e4e38ec562cdf7f27d0bb441135ff216581845eb
unknown <aalouane.s@gmail.com>
2022-08-28 13:47:38 +0000

    Training 28-08-2022 1

--- Chapter 4 Data Manipulation with pandas/5 Creating and Visualizing DataFrames.py
+++ Chapter 4 Data Manipulation with pandas/5 Creating and Visualizing DataFrames.py
@@ -170,3 +170,35 @@ avocados_filled[cols_with_missing].hist()
 plt.show()
 
 ## => Notice how the distribution has changed shape after replacing missing values with zeros.
+
+
+'''
+List of dictionaries
+'''
+# Create a list of dictionaries with new data
+avocados_list = [
+    {"date": "2019-11-03", "small_sold": 10376832, "large_sold": 7835071},
+    {"date": "2019-11-10", "small_sold": 10717154, "large_sold": 8561348},
+]
+
+# Convert list into DataFrame
+avocados_2019 = pd.DataFrame(avocados_list)
+
+# Print the new DataFrame
+print(avocados_2019)
+
+'''
+Dictionary of lists
+'''
+# Create a dictionary of lists with new data
+avocados_dict = {
+  "date": ["2019-11-17", "2019-12-01"],
+  "small_sold": [10859987, 9291631],
+  "large_sold": [7674135, 6238096]
+}
+
+# Convert dictionary into DataFrame
+avocados_2019 = pd.DataFrame(avocados_dict)
+
+# Print the new DataFrame
+print(avocados_2019)
