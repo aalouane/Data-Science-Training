53aa9f6ae73e03bf00b62d61200e24a95fe33f6e
unknown <aalouane.s@gmail.com>
2022-08-29 14:09:10 +0000

    Training 29-08-2022 2

--- Part 5 Joining Data with pandas/1 Data Merging Basics.py
+++ Part 5 Joining Data with pandas/1 Data Merging Basics.py
@@ -118,3 +118,19 @@ few businesses and people around to avoid complaints about the smell. You will n
 The land_use table has info on the percentage of vacant land by city ward. 
 The census table has population by ward, and the licenses table lists businesses by ward.
 '''
+# Merge land_use and census and merge result with licenses including suffixes
+land_cen_lic = land_use.merge(census, on='ward') \
+                    .merge(licenses, on='ward', suffixes=('_cen','_lic'))
+
+# Group by ward, pop_2010, and vacant, then count the # of accounts
+pop_vac_lic = land_cen_lic.groupby(['ward','pop_2010','vacant'], 
+                                   as_index=False).agg({'account':'count'})
+
+# Sort pop_vac_lic and print the results
+sorted_pop_vac_lic = pop_vac_lic.sort_values(['vacant', 'account', 'pop_2010'], 
+                    ascending=[False, True, True])
+
+# Print the top few rows of sorted_pop_vac_lic
+print(sorted_pop_vac_lic.head())
+
+# ==> Using your skills, you were able to pull together information from different tables to see that the 7th ward would be a good place to build your goat farm!
